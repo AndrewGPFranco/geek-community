fa4223c0e1d6ba8e237161ac669aa9a983d69a55
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-07-31 21:59:32 -0300

    test: corrigindo config de testes

--- src/test/java/com/agp/ead/EadApplicationTests.java
+++ src/test/java/com/agp/ead/EadApplicationTests.java
@@ -1,9 +1,11 @@
 package com.agp.ead;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.flyway.enabled=false")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class EadApplicationTests {
 
 	@Test
