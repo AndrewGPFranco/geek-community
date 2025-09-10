63481c2cdcdea3e3411e251cde5997e883273071
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-09-10 07:15:43 -0300

    refatorando método que retorna tags a partir de uma lista de string e nova tag

--- src/main/java/com/agp/geek/enums/TagType.java
+++ src/main/java/com/agp/geek/enums/TagType.java
@@ -2,6 +2,7 @@ package com.agp.geek.enums;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -9,7 +10,8 @@ public enum TagType {
     ANIME("Anime"),
     MANGA("Manga"),
     SERIE("Série"),
-    DESENHO("Desenho");
+    DESENHO("Desenho"),
+    GAME("Jogo");
 
     private final String description;
 
@@ -18,6 +20,28 @@ public enum TagType {
     }
 
     public static List<TagType> getTagsFromStrings(List<String> tagsStr) {
-        return tagsStr.stream().map(TagType::valueOf).toList();
+        List<TagType> tags = new ArrayList<>(tagsStr.size());
+
+        for (String tag : tagsStr) {
+            switch (tag) {
+                case "Anime":
+                    tags.add(ANIME);
+                    break;
+                case "Manga":
+                    tags.add(MANGA);
+                    break;
+                case "Série":
+                    tags.add(SERIE);
+                    break;
+                case "Desenho":
+                    tags.add(DESENHO);
+                    break;
+                case "Jogo":
+                    tags.add(GAME);
+                    break;
+            }
+        }
+
+        return tags;
     }
 }
